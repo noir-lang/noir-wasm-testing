edea4da22a6dc0e35ccb24beb2f63043da862a6d
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 14:51:50 +0100

    Improve script

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
-main_dir=$(pwd)
+#!/bin/bash
 
+HOME=$(pwd)
+NARGO_VERSION="v0.3.2"
+
+# Download and install nargo
 mkdir -p $HOME/.nargo/bin && \
-curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.3.2/nargo-x86_64-unknown-linux-gnu.tar.gz && \
-tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
-echo -e '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.bashrc && \
-source ~/.bashrc
+curl -o $HOME/.nargo/bin/nargo-aarch64-apple-darwin.tar.gz -L https://github.com/noir-lang/noir/releases/download/$NARGO_VERSION/nargo-aarch64-apple-darwin.tar.gz && \
+tar -xvf $HOME/.nargo/bin/nargo-aarch64-apple-darwin.tar.gz -C $HOME/.nargo/bin/ && \
+echo '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.zshrc && \
+source ~/.zshrc
 
-# nargo --help
+# Wait until nargo is installed
+while ! command -v nargo >/dev/null 2>&1; do
+  echo "Waiting for nargo to be installed..."
+  sleep 1
+done
+
+# Display nargo version
+nargo -V
