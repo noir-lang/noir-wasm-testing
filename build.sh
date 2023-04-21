223cea313300578137a3d9d4655d925ba42269e4
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 15:38:44 +0100

    Fix script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | PREFIX=~/.nargo/bin/ bash
+export PATH="$PATH:$HOME/.nargo/bin/"
+curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | bash
 noirup
 nargo -V
