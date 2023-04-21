f792680fa7ffb64cc03a7d1a20e6b40aa95625c3
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 14:55:34 +0100

    Fix script

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ NARGO_VERSION="v0.3.2"
 
 # Download and install nargo
 mkdir -p $HOME/.nargo/bin && \
-curl -o $HOME/.nargo/bin/nargo-aarch64-apple-darwin.tar.gz -L https://github.com/noir-lang/noir/releases/download/$NARGO_VERSION/nargo-aarch64-apple-darwin.tar.gz && \
-tar -xvf $HOME/.nargo/bin/nargo-aarch64-apple-darwin.tar.gz -C $HOME/.nargo/bin/ && \
-echo '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.zshrc && \
-source ~/.zshrc
+curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.3.2/nargo-x86_64-unknown-linux-gnu.tar.gz && \
+tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
+echo -e '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.bashrc && \
+source ~/.bashrc
 
 # Wait until nargo is installed
 while ! command -v nargo >/dev/null 2>&1; do
