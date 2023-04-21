0a4f91aa9b6d0e5abac6ac9a0c0575bac8f0b376
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 16:19:37 +0100

    Add compilation step

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ export PATH="$PATH:$HOME/.nargo/bin/"
 curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | bash
 noirup
 nargo -V
+cd src/nargo-compiler
+nargo compile nargo-compiler
