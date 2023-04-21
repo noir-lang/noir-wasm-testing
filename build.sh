d50538eaadd895f5ae631d31ee9f914e3b7f1bd3
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 14:42:04 +0100

    Runs build script from workflow

---
+++ build.sh
@@ -0,0 +1,9 @@
+main_dir=$(pwd)
+
+mkdir -p $HOME/.nargo/bin && \
+curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.3.2/nargo-x86_64-unknown-linux-gnu.tar.gz && \
+tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
+echo -e '\nexport PATH=$PATH:$HOME/.nargo/bin' >> ~/.bashrc && \
+source ~/.bashrc
+
+nargo --help
