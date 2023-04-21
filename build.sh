437f335029aa2e8e4404b8cbea4f270dc7a31072
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 16:24:08 +0100

    Update nargo version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export PATH="$PATH:$HOME/.nargo/bin/"
 curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | bash
-noirup
+noirup --version 0.4.1
 nargo -V
 cd src/nargo-compiler
 nargo compile nargo-compiler
