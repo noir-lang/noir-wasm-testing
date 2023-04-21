cd612f0850d66c23252b21ee2473baec8a4c089c
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-04-21 14:56:50 +0100

    Fix script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # HOME=$(pwd)
 NARGO_VERSION="v0.3.2"
 
+echo $HOME
+
 # Download and install nargo
 mkdir -p $HOME/.nargo/bin && \
 curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.3.2/nargo-x86_64-unknown-linux-gnu.tar.gz && \
