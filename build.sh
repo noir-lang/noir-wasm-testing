9e78f96e11e05f6ce81e17a17b51677a19dbdb54
TomAFrench <tom@tomfren.ch>
2023-07-28 04:40:32 +0000

    chore: stop installing nargo through noirup

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-export PATH="$PATH:$HOME/.nargo/bin/"
-curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | bash
-noirup -n
 nargo -V
 cd src/noir-script
 nargo compile noir-script
