5ca963d449defe6da24232983ff006335a4c64bc
TomAFrench <tom@tomfren.ch>
2023-07-28 09:39:39 +0000

    chore: add a name to `Nargo.toml`

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export PATH="$PATH:$HOME/.nargo/bin/"
 curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | bash
 noirup -n
-nargo -v
+nargo -V
 cd src/noir-script
 nargo compile noir-script
