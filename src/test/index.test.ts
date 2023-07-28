eef14f6766e5b7d14957aac7d526c03c588d4b18
TomAFrench <tom@tomfren.ch>
2023-07-28 12:26:13 +0000

    chore: switch to acvm_js style testing

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,15 +1,66 @@
-import { html, fixture, expect } from "@open-wc/testing";
-import "../../my-element.js";
+import { expect } from "@esm-bundle/chai";
+import { initialiseResolver } from "@noir-lang/noir-source-resolver";
+import initNoirWasm, { build_info, compile } from "@noir-lang/noir_wasm";
+
+
+async function getFileContent(path: string): Promise<string> {
+  const mainnrSourceURL = new URL(path, import.meta.url);
+  const response = await fetch(mainnrSourceURL);
+  return await response.text();
+}
+
+async function getSource(): Promise<string> {
+  return getFileContent('../noir-script/src/main.nr')
+}
+
+async function getPrecompiledSource(): Promise<string> {
+  const compiledData = await getFileContent('../noir-script/target/noir-script.json');
+  return JSON.parse(compiledData).bytecode;
+}
+
+export async function compileNoirSource(noir_source: string): Promise<any> {
+  await initNoirWasm();
+
+  console.log("Compiling Noir source...");
+
+  initialiseResolver((id: string) => {
+    console.log(`Resolving source ${id}`);
+
+    const source = noir_source;
+
+    if (typeof source === "undefined") {
+      throw Error(`Could not resolve source for '${id}'`);
+    } else {
+      return source;
+    }
+  });
+
+  try {
+    const compiled_noir = compile({});
+
+    console.log("Noir source compilation done.");
+
+    return compiled_noir.circuit;
+  } catch (e) {
+    console.log("Error while compiling:", e);
+  }
+}
+
 
 describe("noir wasm compilation", () => {
-  let element: any;
-
-  before(async () => {
-    element = await fixture(html`<my-element />`);
-    element.shadowRoot.querySelector("button").click();
-  })
-
   it("matches nargos compilation", async () => {
-    expect(await element.promise).to.equal(true);
+    const source = await getSource();
+
+    const wasmCircuitBase64 = await compileNoirSource(source);
+
+    const cliCircuitBase64 = await getPrecompiledSource();
+
+    console.log("wasm", wasmCircuitBase64);
+
+    console.log("cli", cliCircuitBase64);
+
+    console.log("Compilation is a match? ", wasmCircuitBase64 === cliCircuitBase64);
+
+    expect(wasmCircuitBase64).to.equal(cliCircuitBase64);
   }).timeout(10e3);
 });
